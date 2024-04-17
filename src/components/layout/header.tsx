adcda5a0e32c20a720f35acda594a715f5e35ced
Cristian <cristian.coppari@gmail.com>
2024-04-17 02:41:45 -0300

    fix: lighthouse errors and header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -67,58 +67,85 @@ export default function Header({ leftNavKeys, rightNavKeys, destinationsKeys }: 
       </div>
       <div className="bg-black/90">
         <div className={cn("container mx-auto flex h-16 items-center justify-between md:h-24 md:justify-center")}>
-          <Link href="/" className="block md:hidden">
+          <Link href="/" className="block md:hidden" aria-label="logo">
             <img src="/images/logo-gray.png" alt="" className="h-auto w-20" />
           </Link>
 
           <Burger classes="block md:hidden" onClick={toggleMenu} isOpen={isMenuOpen} />
 
-          <ul
+          <div
             className={cn(
               "absolute right-0 top-24 z-20 hidden w-full flex-col justify-around transition-colors duration-500 md:relative md:top-0 md:flex md:h-full md:flex-row md:items-center md:border-none",
               isMenuOpen && "flex bg-black",
             )}
           >
-            {leftNavKeys.map((link) => {
-              if (link.href === "/destinos") {
-                return (
-                  <li
-                    className="relative grid place-items-center p-4 text-sm font-light uppercase text-slate-200 transition-all duration-300 ease-in-out hover:text-slate-100 md:h-full md:p-0 md:pt-4"
-                    key={link.label}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                  >
-                    <div className={cn(pathname === "/destinos" && "--active")}>
-                      <span>{link.label}</span>
-                      <Arrow classes="w-14 block mx-auto mt-1 arrow" />
-                    </div>
-                    <motion.ul
-                      className="top-full flex flex-col items-center bg-black md:absolute md:items-start"
-                      variants={dropdownVariants}
-                      initial="hidden"
-                      animate={isHovered ? "visible" : "hidden"}
+            <ul className="flex flex-1 flex-col justify-evenly md:flex-row">
+              {leftNavKeys.map((link) => {
+                if (link.href === "/destinos") {
+                  return (
+                    <li
+                      className="relative grid place-items-center p-4 text-sm font-light uppercase text-slate-200 transition-all duration-300 ease-in-out hover:text-slate-100 md:h-full md:p-0 md:pt-4"
+                      key={link.label}
+                      onMouseEnter={() => setIsHovered(true)}
+                      onMouseLeave={() => setIsHovered(false)}
                     >
-                      {destinationsKeys.map((item) => (
-                        <li
-                          className="w-40 px-4 py-2 text-sm font-light uppercase text-slate-200 opacity-80 transition-all duration-300 ease-in-out hover:text-slate-100"
-                          key={item.label}
-                        >
-                          <Link
-                            href={item.href}
-                            className={cn(
-                              "nav-link grid place-items-center",
-                              // `/${router.query.destino as string}` === `/${item.href.split("/")[2]}` && "--active",
-                            )}
+                      <div className={cn(pathname === "/destinos" && "--active")}>
+                        <span>{link.label}</span>
+                        <Arrow classes="w-14 block mx-auto mt-1 arrow" />
+                      </div>
+                      <motion.ul
+                        className="top-full flex flex-col items-center bg-black md:absolute md:items-start"
+                        variants={dropdownVariants}
+                        initial="hidden"
+                        animate={isHovered ? "visible" : "hidden"}
+                      >
+                        {destinationsKeys.map((item) => (
+                          <li
+                            className="w-40 px-4 py-2 text-sm font-light uppercase text-slate-200 opacity-80 transition-all duration-300 ease-in-out hover:text-slate-100"
+                            key={item.label}
                           >
-                            <span>{item.label}</span>
-                            <Arrow classes="w-14 block mx-auto mt-1 arrow" />
-                          </Link>
-                        </li>
-                      ))}
-                    </motion.ul>
-                  </li>
-                );
-              } else {
+                            <Link
+                              href={item.href}
+                              className={cn(
+                                "nav-link grid place-items-center",
+                                // `/${router.query.destino as string}` === `/${item.href.split("/")[2]}` && "--active",
+                              )}
+                              aria-label="navbar link"
+                            >
+                              <span>{item.label}</span>
+                              <Arrow classes="w-14 block mx-auto mt-1 arrow" />
+                            </Link>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li className="grid place-items-center p-4 text-center md:p-0 md:pt-4" key={link.label}>
+                      <Link
+                        href={link.href}
+                        className={cn(
+                          "nav-link text-sm font-light uppercase text-slate-200 transition-all duration-300 ease-in-out hover:text-slate-100",
+                          pathname === link.href && "--active",
+                        )}
+                        aria-label="navbar link"
+                      >
+                        <span>{link.label}</span>
+                        <Arrow classes="w-14 block mx-auto mt-1 arrow" />
+                      </Link>
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+
+            <Link href="/" className="hidden md:block">
+              <img src="/images/logo-gray.png" alt="" className="h-auto w-20" />
+            </Link>
+
+            <ul className="flex flex-1 flex-col justify-evenly md:flex-row">
+              {rightNavKeys.map((link) => {
                 return (
                   <li className="grid place-items-center p-4 text-center md:p-0 md:pt-4" key={link.label}>
                     <Link
@@ -133,30 +160,9 @@ export default function Header({ leftNavKeys, rightNavKeys, destinationsKeys }: 
                     </Link>
                   </li>
                 );
-              }
-            })}
-
-            <Link href="/" className="hidden md:block">
-              <img src="/images/logo-gray.png" alt="" className="h-auto w-20" />
-            </Link>
-
-            {rightNavKeys.map((link) => {
-              return (
-                <li className="grid place-items-center p-4 text-center md:p-0 md:pt-4" key={link.label}>
-                  <Link
-                    href={link.href}
-                    className={cn(
-                      "nav-link text-sm font-light uppercase text-slate-200 transition-all duration-300 ease-in-out hover:text-slate-100",
-                      pathname === link.href && "--active",
-                    )}
-                  >
-                    <span>{link.label}</span>
-                    <Arrow classes="w-14 block mx-auto mt-1 arrow" />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </header>
